1eeb07a82702840a4ee5eb4ad7e8a87362e05686
roshank8848 <68323843+roshank8848@users.noreply.github.com>
2026-05-29 09:41:43 +0000

    feat: enable file and remote-exec provisioner

---
+++ modules/compute/install_wordpress.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# modules/compute/install_wordpress.sh
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# 1. Update system and install Apache & PHP 8.x
+sudo apt-get update -y
+sudo apt-get install -y apache2 php libapache2-mod-php php-mysql php-curl php-gd php-mbstring php-xml php-xmlrpc php-soap php-intl php-zip mariadb-client
+
+# 2. Restart Apache to load PHP
+sudo systemctl enable apache2
+sudo systemctl start apache2
+
+# 3. Download and extract WordPress
+cd /tmp
+wget https://wordpress.org/latest.tar.gz
+tar -xzf latest.tar.gz
+
+# 4. Configure wp-config.php using the passed environment variables
+cd wordpress
+# cp wp-config-sample.php wp-config.php
+
+# sed -i "s/database_name_here/$DB_NAME/g" wp-config.php
+# sed -i "s/username_here/$DB_USER/g" wp-config.php
+# sed -i "s/password_here/$DB_PASS/g" wp-config.php
+# sed -i "s/localhost/$DB_HOST/g" wp-config.php
+
+# 5. Clean default Apache index and move WordPress files to web root
+sudo rm -rf /var/www/html/index.html
+sudo cp -r * /var/www/html/
+
+# 6. Set proper permissions for Apache user
+# sudo chown -w /var/www/html/
+sudo chown -R www-data:www-data /var/www/html/
+sudo chmod -R 755 /var/www/html/
+
+# 7. Restart Apache one last time
+sudo systemctl restart apache2
--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -6,9 +6,9 @@ resource "aws_security_group" "rds_sg" {
 
   # Strict firewall rule: Only allow connections on 5432 from the Public VM's Security Group
   ingress {
-    description = "PostgreSQL from Public Web Server"
-    from_port   = 5432
-    to_port     = 5432
+    description = "Mysql from Public Web Server"
+    from_port   = 3306
+    to_port     = 3306
     protocol    = "tcp"
     cidr_blocks = [data.aws_vpc.selected.cidr_block]
   }
@@ -29,9 +29,9 @@ data "aws_vpc" "selected" {
 
 # Free-Tier AWS RDS PostgreSQL Instance
 resource "aws_db_instance" "postgres_db" {
-  identifier                 = "${var.environment}-postgres"
-  engine                     = "postgres"
-  engine_version             = "16"
+  identifier                 = "${var.environment}-mysql"
+  engine                     = "mysql"
+  engine_version             = "8.0"
   auto_minor_version_upgrade = true
   instance_class             = "db.t3.micro" # AWS Free Tier eligible
   allocated_storage          = 20            # 20 GB (Within 30GB free limit)
